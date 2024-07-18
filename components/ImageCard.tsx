2c05e07ea62b27bab0f1c1419001fec2c7f7c9d7
itmadityajoshi <adityajoshi316@gmail.com>
2024-07-18 12:21:24 +0545

    styling done in cards

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -5,21 +5,30 @@ const ImageCard = () => {
   return (
     <View>
       <Text style={styles.headingText}>ImageCard</Text>
-      <View style={styles.container}>
+      <View style={[styles.container, styles.elevated]}>
         <Image
           source={{
             uri: 'https://media.istockphoto.com/id/480044006/photo/bhaktapur-durbar-square.webp?b=1&s=170667a&w=0&k=20&c=bGvzplTWooJkOCf-VVs7j55xzW3u2FYXc7_KWauL56k=',
           }}
           style={styles.cardImage}
         />
-      </View>
-      <View style={styles.cardDetail}>
-        <Text style={[styles.title]}>Bhaktapur Darbar Square</Text>
-        <Text style={[styles.cardlabel]}>The city is also known for its Newar tradition, cuisine and artisans.</Text>
-        <Text style={[styles.description]}>Bhaktapur's oldest hiti is also dated from the Licchavi dynasty.</Text>
-        <Text style={[styles.footer]}>Bhaktapur, Nepal</Text>
 
+        <View style={styles.cardDetail}>
+          <Text style={[styles.title]}>Bhaktapur Darbar Square</Text>
+          <Text style={[styles.cardlabel]}>
+            The city is also known for its Newar tradition, cuisine and
+            artisans.
+          </Text>
+          <Text style={[styles.description]}>
+            Bhaktapur's oldest hiti is also dated from the Licchavi dynasty.
+          </Text>
+          <Text style={[styles.footer]}>Bhaktapur, Nepal</Text>
+        </View>
       </View>
+
+
+        
+
     </View>
   );
 };
@@ -27,35 +36,48 @@ const ImageCard = () => {
 export default ImageCard;
 
 const styles = StyleSheet.create({
+  main: {},
   headingText: {
     fontSize: 24,
     fontWeight: 'bold',
     paddingLeft: 8,
   },
-  container: {},
+  container: {
+    width: 350,
+    height: 400,
+    borderRadius: 6,
+    marginVertical: 16,
+    marginHorizontal: 20,
+  },
+  elevated: {
+    elevation: 4,
+    backgroundColor: '#C6DDF0',
+  },
   cardImage: {
     height: 250,
-    margin: 6,
-    borderRadius:8,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+    marginBottom: 10,
   },
-  title:{
+  cardDetail: {
+    paddingHorizontal: 10,
+  },
+  title: {
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: 5,
+    fontSize: 24,
   },
-  description:{
-    fontStyle: 'italic'
+  description: {
+    fontStyle: 'italic',
+    flexShrink: 1,
   },
-  cardlabel:{
+  cardlabel: {
     fontStyle: 'italic',
   },
-  footer:{
+  footer: {
     textAlign: 'center',
     margin: 10,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-  cardDetail:{
-    backgroundColor:'gold',
-    margin: 8,
-    borderRadius: 8,
-  }
 });
